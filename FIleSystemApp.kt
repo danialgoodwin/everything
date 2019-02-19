19a950a1a7d0a4a91f5ab4ac2303178dbb67c7b8
Danial Goodwin <danialgoodwin@gmail.com>
2019-02-18 23:22:36 -0700

    Create FIleSystemApp.kt

---
+++ FIleSystemApp.kt
@@ -0,0 +1,15 @@
+
+object FileSystemApp {
+ 
+}
+
+class FileSystemRepo {
+
+}
+
+data class Path() { }
+
+data class File(val name: String, val permission: Permission) { }
+
+data class Permission() { }
+
